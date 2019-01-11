9eaeb08b3e20f8ca4421ed67ff0e01c8a80b7cb0
lijiaxiong294223 <45058844+lijiaxiong294223@users.noreply.github.com>
2019-01-11 15:08:49 +0800

    qqqq

---
+++ src/main/java/com/zhxy/service/Service_Studentassinment.java
@@ -0,0 +1,10 @@
+package com.zhxy.service;
+
+import java.util.List;
+
+import com.zhxy.domain.Studentassignment;
+
+public interface Service_Studentassinment {
+
+	public List<Studentassignment> stuquery();
+}
