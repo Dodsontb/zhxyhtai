69ca3d15533db5d11db39f9737c8816572739896
zkmhy <45059168+zkmhy@users.noreply.github.com>
2019-01-11 11:18:22 +0800

    配置

---
+++ src/main/java/com/zhxy/controller/WriteController.java
@@ -0,0 +1,13 @@
+package com.zhxy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WriteController {
+
+	@RequestMapping("/write")
+	public String write() {
+		return "write";
+	}
+}
