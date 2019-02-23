5fb53dbab142daca3d6431602428a9288f36def4
lijiaxiong294223 <45058844+lijiaxiong294223@users.noreply.github.com>
2019-02-23 11:20:00 +0800

    2019-2-23 11:19

--- src/main/java/com/zhxy/ZhxyApplication.java
+++ src/main/java/com/zhxy/ZhxyApplication.java
@@ -1,9 +1,10 @@
 package com.zhxy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@MapperScan("com.zhxy.dao")
 @SpringBootApplication
 @EnableScheduling
 public class ZhxyApplication {
