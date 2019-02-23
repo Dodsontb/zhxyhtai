642b1836d74a2c8f3eca7b7ae8a195051a683ef7
鹏鹏鹏 <1831206959@qq.com>
2019-02-23 14:40:45 +0800

    ...

--- src/main/java/com/zhxy/controller/UO.java
+++ src/main/java/com/zhxy/controller/UO.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -70,7 +71,8 @@ public class UO {
 	}
 	
 	@RequestMapping("cp_select")
-	public String cp_select() {
+	public String cp_select(String templateid,Model model) {
+		model.addAttribute("templateid",templateid);
 		return "cp_select";
 	}
 	
